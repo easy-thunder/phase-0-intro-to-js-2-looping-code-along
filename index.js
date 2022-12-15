89a5e6ead44d93bdee00c75cd81a1179020b4a55
Jacob David Diehl <jakediehl17@gmail.com>
2022-12-15 10:31:14 -0700

    commit tree abe191078c9db260e065a883ff0827b21aad6568

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const names =["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names){
+    const newNames = [];
+    for(let i = 0; i<names.length; i++){
+        
+
+
+        newNames.push( `Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+        return newNames;
+    
+}
+
+writeCards(names)
+
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
+
+
+
+
